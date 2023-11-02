7484fdd8135ae1cc78a6106b0c771d68ddb1e08e
RamdlanFaqih <ramfaqih@gmail.com>
2023-11-02 11:09:18 +0700

    add integration between users &  my_recipes

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -4,20 +4,30 @@ import Navigation from "../../Component/Navigation/Nav1";
 import Footer from "../../Component/Footer/Footer";
 import Button from "../../Component/Button/Button";
 import style from "./profile.module.css";
+import Image1 from "../../assets/img/img1.svg";
 
 import ModalUpdate from "../../Component/modalUpdate/index";
 
 const Profile = () => {
   const [user, setUser] = React.useState("");
-  const userId = localStorage.getItem("userId")
-  console.log(userId);
+  const [recipes, setRecipes] = React.useState([]);
+  const userID = localStorage.getItem("userID");
+  console.log(userID);
 
   React.useEffect(() => {
     const fetchUser = async () => {
       try {
-        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/users/${userId}`);
-        console.log(response.data.data.rows[0]);
-        setUser(response.data.data.rows[0]);
+        const response = await axios.get(
+          `${process.env.REACT_APP_BACKEND_URL}/users/recipes/${userID}`
+        );
+        console.log(response.data.rows);
+        setUser(response.data.rows[0]);
+
+        const recipesResponse = await axios.get(
+          `${process.env.REACT_APP_BACKEND_URL}/recipes/users/${userID}`
+        );
+        console.log(recipesResponse.data.data.rows);
+        setRecipes(recipesResponse.data.data.rows);
       } catch (error) {
         console.error("Failed to fetch user:", error);
       }
@@ -25,19 +35,26 @@ const Profile = () => {
 
     fetchUser();
   }, []);
+  React.useEffect(() => {
+    localStorage.setItem("userRecipes", JSON.stringify(recipes));
+  }, [recipes]);
 
   return (
     <div>
       <Navigation />
       <div className="container" id="allContent">
+        {console.log(user)}
         <div className="row align-items-center">
-          <div className="col-12 text-center" id="display-picture">
-            <img src={user.image} className="img-fluid" alt="" />
+          <div className={`col-12 text-center`}>
+            <img src={user.user_image} className={`${style.image}`} alt="" />
             <ModalUpdate />
-            <h1>{user.name}</h1>
+            <h1>{user.user_name}</h1>
           </div>
-          <div className="col-12 text-center">
-            <div className="accordion accordion-flush" id="accordionFlushExample">
+          <div className="col-12">
+            <div
+              className="accordion accordion-flush"
+              id="accordionFlushExample"
+            >
               <div className="accordion-item">
                 <h2 className="accordion-header">
                   <button
@@ -56,10 +73,19 @@ const Profile = () => {
                   className="accordion-collapse collapse"
                   data-bs-parent="#accordionFlushExample"
                 >
-                  <div className="accordion-body">
-                    Placeholder content for this accordion, which is intended to
-                    demonstrate the <code>.accordion-flush</code> class. This is
-                    the first item's accordion body.
+                  <div className="accordion-body row">
+                    {recipes.map((recipe) => (
+                      <div className="col col-lg-4" key={recipe.recipes_id}>
+                        <div className={style.recipesContainer}>
+                          <img
+                            src={recipe.image}
+                            alt="myrecipe"
+                            className={style.recipes}
+                          />
+                          <h1 className={style.foodName}>{recipe.food_name}</h1>
+                        </div>
+                      </div>
+                    ))}
                   </div>
                 </div>
               </div>
