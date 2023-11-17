54b019f4c92322026802d6cb7c9d349439407ad5
RamdlanFaqih <ramfaqih@gmail.com>
2023-11-17 13:39:27 +0700

    logging add recipe

--- src/views/AddRecipe/index.js
+++ src/views/AddRecipe/index.js
@@ -24,6 +24,7 @@ const AddRecipe = () => {
       ...data,
       [e.target.name]: value,
     });
+    console.log(data);
   };
   const handleUpload = (e) => {
     const uploader = e.target.files[0];
